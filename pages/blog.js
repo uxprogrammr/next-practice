1d20e375c1c232e1d54a3d409477add9f06a410a
Ivan Lim <uxprogrammer@gmail.com>
2022-05-10 00:17:44 +0800

    display json contents

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,14 @@
+import posts from "../posts.json";
+
 export default function Blog() {
   return (
     <div>
       <h1>Blog</h1>
+      <ul>
+        {Object.keys(posts).map((id, index) => {
+          return <li key={index}>{posts[id].title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
