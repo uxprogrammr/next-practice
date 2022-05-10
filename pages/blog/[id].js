926307e092e0e6119c9d86c51d6ab7a35606b3a7
Ivan Lim <uxprogrammer@gmail.com>
2022-05-11 02:05:20 +0800

    static data fetching

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,16 +1,27 @@
-import { useRouter } from "next/router";
 import posts from "../../posts.json";
 
-export default function BlogPost() {
-  const router = useRouter();
-
-  const post = posts[router.query.id];
-  if (!post) return <p>Could not found!</p>;
-
+export default function BlogPost({ post }) {
   return (
     <>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
     </>
   );
+}
+
+export async function getStaticPaths() {
+  console.log("getStaticPaths");
+  return {
+    paths: Object.keys(posts).map((id) => ({ params: { id } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  console.log("getStaticProps");
+  return {
+    props: {
+      post: posts[params.id],
+    },
+  };
 }
