466739b04f49b6dd98ffafec337720b232702b03
Ivan Lim <uxprogrammer@gmail.com>
2022-05-10 23:22:05 +0800

    add DogImage component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import DogImage from "./dog";
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <DogImage />
       <Link href="/blog">
         <a>Blog</a>
       </Link>
